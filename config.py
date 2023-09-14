55380ca638a65394c9db7d4395d18df9cc1f11c9
Asher Bolleddu <104097139+AsherBolleddu@users.noreply.github.com>
2023-09-14 18:27:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API_KEY = "f12956d5109ff025a9bc97ba0f1e"
-sourceUrl = "https://candidate.hubteam.com/candidateTest/v3/problem/dataset?userKey=f12956d5109ff025a9bc97ba0f1e"
-targetUrl = "https://candidate.hubteam.com/candidateTest/v3/problem/result?userKey=f12956d5109ff025a9bc97ba0f1e"
+API_KEY = <YOUR_API_KEY_HERE>
+sourceUrl =
+targetUrl =
 TEN_MINUTES = 10 * 60 * 1000
